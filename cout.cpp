9704e0b1c1e3ea31c0776e5a8fedeabdc6731850
Yuuseo <bayubysn@gmail.com>
2026-03-09 23:07:56 +0700

    Add files via upload

---
+++ cout.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Nama saya Bayu Setyo Nugroho" << endl;
+    return 0;
+}
---
+++ puts.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    puts("Nama Saya Bayu Setyo Nugroho");
+    return 0;
+}
